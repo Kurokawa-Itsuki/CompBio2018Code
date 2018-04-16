fab1bbc6a2b8adda11169b83641f04511f174031
Kurokawa-Itsuki <38201019+Kurokawa-Itsuki@users.noreply.github.com>
2018-04-16 15:23:54 +0900

    2-4update

---
+++ 02/kadai2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(int argc, char const *argv[]) {
+  int t,k,r,x;
+  t=;
+  k=;
+  r=;
+  x=;
+
+  FILE fp*;
+  fp=fopen("output.dat");
+
+  fprintf(fp,"%d, %d",0,x0);
+
+  for(t=0;t<300,i++){
+    
+  }
+  return 0;
+}
