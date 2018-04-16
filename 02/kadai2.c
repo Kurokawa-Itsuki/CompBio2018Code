753a02d4ece770384bdbee2a476cc70c497dbac0
Kurokawa-Itsuki <38201019+Kurokawa-Itsuki@users.noreply.github.com>
2018-04-16 17:10:16 +0900

    kadai2update

--- 02/kadai2.c
+++ 02/kadai2.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 int main(int argc, char const *argv[]) {
-  int t,k,r,x;
-  t=;
-  k=;
-  r=;
-  x=;
+  double k,r,x;
+  int t;
+  FILE *fp;
+  t=0;
+  k=100.0;
+  r=3.0;
+  x=1.0;
 
-  FILE fp*;
-  fp=fopen("output.dat");
+  fp=fopen("output.dat","a");
 
-  fprintf(fp,"%d, %d",0,x0);
-
-  for(t=0;t<300,i++){
-  xx=x+r*(1-x/k)*x;
-  }
-
-  fprintf(fp,"");
+  for(t=0;t<300;t++){
+  x=x+r*(1-x/k)*x;
+  fprintf(fp,"%f\n",x);
+}
+fclose(fp);
   return 0;
 }
